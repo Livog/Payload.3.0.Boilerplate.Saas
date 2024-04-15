63d39e2a1c992a9fa2bdffa83f06ee6cfcc3db07
Fredrik Nordström <livog@live.com>
2024-04-15 20:43:24 +0200

    Add Session Provider

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Providers from './providers'
 import '@/app/style.css'
+import { auth } from '@/lib/auth'
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Layout: React.FC<{ children: React.ReactNode }> = async ({ children }) => {
+  const session = await auth()
   return (
-    <html>
-      <body>{children}</body>
-    </html>
+    <Providers session={session}>
+      <html>
+        <body>{children}</body>
+      </html>
+    </Providers>
   )
 }
 
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -2,7 +2,6 @@ import { OAuthLoginButtons } from '@/components/OAuthLoginButtons'
 import { auth } from '@/lib/auth'
 
 const Page = async () => {
-  const session = await auth()
   return (
     <article>
       <OAuthLoginButtons />
